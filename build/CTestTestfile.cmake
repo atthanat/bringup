40694ba433c059de8868c96e00639d6f444f51d3
Atthanat <atthanath_pro@vistec.ac.th>
2021-03-31 09:13:33 +0700

    add file

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/snow/bringup/src
+# Build directory: /home/snow/bringup/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("ai_camp_bringup")
+subdirs("usb_cam")
---
+++ build/ai_camp_bringup/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/snow/bringup/src/ai_camp_bringup
+# Build directory: /home/snow/bringup/build/ai_camp_bringup
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
